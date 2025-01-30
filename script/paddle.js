f13f75fc0cb1112118f9619ae0c29fa38fb16630
SnowMar <mnagy78951@gmail.com>
2025-01-30 22:07:01 +0200

    Paddle Update

---
+++ script/paddle.js
@@ -0,0 +1,45 @@
+import { canvas, ctx } from './canvas.js';
+
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+function keyDownHandler(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === "Right" || e.key === "ArrowRight" || e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = 0;
+  }
+}
+
+export const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 10,
+  width: 80,
+  height: 10,
+  color: "green",
+  speed: 8,
+  dx: 0,
+  draw() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    ctx.fillStyle = paddle.color;
+    ctx.fill();
+    ctx.closePath();
+  },
+  updatePaddle() {
+    paddle.x += paddle.dx;
+  
+    if (paddle.x < 0) {
+      paddle.x = 0;
+      } else if (paddle.x + paddle.width > canvas.width) {
+        paddle.x = canvas.width - paddle.width;
+      }
+    }
+};
+
