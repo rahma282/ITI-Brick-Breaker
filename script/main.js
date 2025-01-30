c126101d34c9cef9d4512e2fef2d0917017e3b78
Rahma Mostafa <rahmamostafa282@gmail.com>
2025-01-30 22:56:08 +0200

    add score and lives modules

--- script/main.js
+++ script/main.js
@@ -1,1 +1,9 @@
 import { canvas, ctx } from './canvas.js';
+import { score } from './score.js'
+import { lives } from './lives.js'
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    score.draw();
+    lives.draw();
+}
+setInterval(draw, 10);
