03722eb000287bafb11346ebafe3de0d2a09bcf8
Arwa-Mohamed23 <arwamohmd.32@gmail.com>
2025-02-01 23:03:06 +0200

    update-2

--- script/endGame.js
+++ script/endGame.js
@@ -1,11 +1,8 @@
-// import { gameState } from './main.js';
-
 let gameModel = document.querySelector(".game-model");
+
 export function endGame(won,score){
-    // gameState.gameOver = true;
     gameModel.style.display = "flex";
     gameModel.querySelector(".your-score").innerText=`${score}`;
     const resultText = gameModel.querySelector(".content h2");
     resultText.innerText = won ? `You Win!`: `GAME OVER!`;
-    // document.getElementById("resetGame").style.display = "block";
 }
